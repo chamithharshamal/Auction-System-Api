94e2ad97d0976eebc6efd569f5d11168fa4c3d93
Chamith Harshamal <chamithweerasinghe1@gmail.com>
2025-08-09 00:40:55 +0530

    Initial commit: Spring Boot auction app with MongoDB setup

---
+++ src/main/java/com/springboot_projects/auction_app_api/AuctionAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.springboot_projects.auction_app_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionAppApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot_projects/auction_app_api/AuctionAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot_projects.auction_app_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
